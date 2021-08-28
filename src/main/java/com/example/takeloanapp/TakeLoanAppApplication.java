78d7481868cd8ceb70d52f8a71d831f7c306de66
adam-p <77075408+adam-pietka@users.noreply.github.com>
2021-08-28 14:40:52 +0200

    second commit

---
+++ src/main/java/com/example/takeloanapp/TakeLoanAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.takeloanapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeLoanAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TakeLoanAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/takeloanapp/TakeLoanAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.takeloanapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeLoanAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
